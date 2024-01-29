203bea3d36adf9d0fabecc7c9ec134536a4a090b
Zhexuan Chen <2915234902@qq.com>
2024-01-29 23:45:41 +0800

    straight-line.lisp: Rewrite to use ADT

--- straight-line.lisp
+++ straight-line.lisp
@@ -24,176 +24,65 @@
 
 (cl:in-package :cl-tiger/straight-line)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (deftype id () 'string)
+(deftype id () 'string)
 
-  (defclass stm ()
-    ())
+(serapeum:defunion op
+  plus-op
+  minus-op
+  times-op
+  div-op)
 
-  (defclass compound-stm (stm)
-    ((first-stm
-      :type stm
-      :initarg :first-stm
-      :accessor compound-stm-first)
-     (rest-stm
-      :type stm
-      :initarg :rest-stm
-      :accessor compound-stm-rest)))
+(serapeum:defunion stm
+  (compound-stm
+   (first stm)
+   (rest stm))
+  (assign-stm
+   (id id)
+   (expr expr))
+  (print-stm
+   ;; A list of expr.
+   (exprs list)))
 
-  (defclass expr ()
-    ())
-
-  (defclass assign-stm (stm)
-    ((id
-      :type id
-      :initarg :id
-      :accessor assign-stm-id)
-     (expr
-      :type expr
-      :initarg :expr
-      :accessor assign-stm-expr)))
-
-  (defclass print-stm (stm)
-    ((exprs
-      :type list
-      :initform nil
-      :initarg :exprs
-      :accessor print-stm-exprs)))
-
-  (defclass id-expr (expr)
-    ((id
-      :type id
-      :initarg :id
-      :accessor id-expr-id)))
-
-  (defclass num-expr (expr)
-    ((num
-      :type number
-      :initarg :num
-      :accessor num-expr-num)))
-
-  (deftype bin-op () '(member :plus :minus :times :div))
-
-  (defclass bin-op-expr (expr)
-    ((left-expr
-      :type expr
-      :initarg :left-expr
-      :accessor bin-op-expr-left)
-     (right-expr
-      :type expr
-      :initarg :right-expr
-      :accessor bin-op-expr-right)
-     (op :type bin-op
-       :initarg :op
-       :accessor bin-op-expr-op)))
-
-  (defclass stm-expr-expr (expr)
-    ((stm
-      :type stm
-      :initarg :stm
-      :accessor stm-expr-expr-stm)
-     (expr
-      :type expr
-      :initarg :expr
-      :accessor stm-expr-expr-expr))))
-
-(defmethod print-object ((stm compound-stm) stream)
-  (format stream "~A; ~A"
-          (compound-stm-first stm)
-          (compound-stm-rest stm)))
-
-(defun make-compound-stm (first-stm rest-stm)
-  (make-instance 'compound-stm
-                 :first-stm first-stm
-                 :rest-stm rest-stm))
-
-(defmethod print-object ((stm assign-stm) stream)
-  (format stream "~A = ~A"
-          (assign-stm-id stm)
-          (assign-stm-expr stm)))
-
-(defun make-assign-stm (id expr)
-  (make-instance 'assign-stm
-                 :id id
-                 :expr expr))
-
-(defmethod print-object ((stm print-stm) stream)
-  (format stream "print(~{~A~^, ~})" (print-stm-exprs stm)))
-
-(defun make-print-stm (exprs)
-  (make-instance 'print-stm :exprs exprs))
-
-(defun make-print-stm* (&rest exprs)
-  (make-print-stm exprs))
-
-(defmethod print-object ((expr id-expr) stream)
-  (format stream "expr(~A)" (id-expr-id expr)))
-
-(defun make-id-expr (id)
-  (make-instance 'id-expr :id id))
-
-(defmethod print-object ((expr num-expr) stream)
-  (format stream "expr(~A)" (num-expr-num expr)))
-
-(defun make-num-expr (num)
-  (make-instance 'num-expr :num num))
-
-(defmethod print-object ((expr bin-op-expr) stream)
-  (format stream "~A ~A ~A"
-          (bin-op-expr-left expr)
-          (ecase (bin-op-expr-op expr)
-            (:plus "+")
-            (:minus "-")
-            (:times "*")
-            (:div "/"))
-          (bin-op-expr-right expr)))
-
-(defun make-bin-op-expr (left-expr op right-expr)
-  (make-instance 'bin-op-expr
-                 :left-expr left-expr
-                 :op op
-                 :right-expr right-expr))
-
-(defmethod print-object ((expr stm-expr-expr) stream)
-  (format stream "(~A, ~A)"
-          (stm-expr-expr-stm expr)
-          (stm-expr-expr-expr expr)))
-
-(defun make-stm-expr-expr (stm expr)
-  (make-instance 'stm-expr-expr :stm stm :expr expr))
+(serapeum:defunion expr
+  (id-expr
+   (id id))
+  (num-expr
+   (num integer))
+  (op-expr
+   (left expr)
+   (op op)
+   (right expr))
+  (stm-expr
+   (stm stm)
+   (expr expr)))
 
 (defvar *prog*
-  (make-compound-stm
-   (make-assign-stm
-    "a"
-    (make-bin-op-expr (make-num-expr 5) :plus (make-num-expr 3)))
-   (make-compound-stm
-    (make-assign-stm
-     "b"
-     (make-stm-expr-expr
-      (make-print-stm*
-       (make-id-expr "a")
-       (make-bin-op-expr (make-id-expr "a") :minus (make-num-expr 1)))
-      (make-bin-op-expr (make-num-expr 10) :times (make-id-expr "a"))))
-    (make-print-stm* (make-id-expr "b")))))
+  (compound-stm
+   (assign-stm "a" (op-expr (num-expr 5) plus-op (num-expr 3)))
+   (compound-stm
+    (assign-stm "b"
+                (stm-expr
+                 (print-stm
+                  (list (id-expr "a")
+                        (op-expr (id-expr "a") minus-op (num-expr 1))))
+                 (op-expr (num-expr 10) times-op (id-expr "a"))))
+    (print-stm (list (id-expr "b"))))))
 
 (defun interpret-stm (stm env)
   ;; Returns another assoc list as new env.
-  (trivia:ematch stm
-    ((compound-stm (compound-stm-first first-stm)
-                   (compound-stm-rest rest-stm))
-     (let ((new-env (interpret-stm first-stm env)))
-       (interpret-stm rest-stm new-env)))
-    ((assign-stm (assign-stm-id id)
-                 (assign-stm-expr expr))
-     (trivia:let-match1 (list int-value new-env) (interpret-expr expr env)
+  (serapeum:match-of stm stm
+    ((compound-stm first rest)
+     (let ((new-env (interpret-stm first env)))
+       (interpret-stm rest new-env)))
+    ((assign-stm id expr)
+     (trivia:let-match (((list int-value new-env) (interpret-expr expr env)))
        (cons (cons id int-value) new-env)))
-    ((print-stm (print-stm-exprs exprs))
+    ((print-stm exprs)
      (let ((first-expr? t))
        (prog1
            (reduce (lambda (cur-env expr)
-                     (trivia:let-match1 (list int-value new-env) (interpret-expr expr cur-env)
-                       (format t (if first-expr? "~A" " ~A") int-value)
+                     (trivia:let-match (((list int-value new-env) (interpret-expr expr cur-env)))
+                       (format t (if  first-expr? "~A" " ~A") int-value)
                        (setf first-expr? nil)
                        new-env))
                    exprs
@@ -202,28 +91,25 @@
 
 (defun interpret-expr (expr env)
   ;; Returns (int-return-value new-env)
-  (trivia:ematch expr
-    ((id-expr (id-expr-id id))
+  (serapeum:match-of expr expr
+    ((id-expr id)
      (let ((id-value (assoc id env :test #'string=)))
        (unless id-value (error "Undefined variable: ~A, current environment: ~A." id env))
        (list (cdr id-value) env)))
-    ((num-expr (num-expr-num num))
+    ((num-expr num)
      (list num env))
-    ((bin-op-expr (bin-op-expr-left left-expr)
-                  (bin-op-expr-op op)
-                  (bin-op-expr-right right-expr))
-     (trivia:let-match* (((list int-value-1 new-env-1) (interpret-expr left-expr env))
-                         ((list int-value-2 new-env-2) (interpret-expr right-expr new-env-1)))
+    ((op-expr left op right)
+     (trivia:let-match* (((list int-value-1 new-env-1) (interpret-expr left env))
+                         ((list int-value-2 new-env-2) (interpret-expr right new-env-1)))
        (list (funcall
-              (ecase op
-                (:plus #'+)
-                (:minus #'-)
-                (:times #'*)
-                (:div #'/))
+              (serapeum:match-of op op
+                  (plus-op #'+)
+                  (minus-op #'-)
+                  (times-op #'*)
+                  (div-op #'/))
               int-value-1 int-value-2)
              new-env-2)))
-    ((stm-expr-expr (stm-expr-expr-stm stm)
-                    (stm-expr-expr-expr expr))
+    ((stm-expr stm expr)
      (let ((new-env (interpret-stm stm env)))
        (interpret-expr expr new-env)))))
 
